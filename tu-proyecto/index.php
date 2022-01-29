b530f54305a3fe2f79fc1499252f85907a3eb9fa
Jhamilchar <jhamil.arroyo.a@gmail.com>
2022-01-29 16:15:16 -0500

    Fechas en php

--- tu-proyecto/index.php
+++ tu-proyecto/index.php
@@ -5,6 +5,9 @@
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 
+setlocale( LC_TIME, 'es', 'spa', 'es_ES');
+date_default_timezone_set( 'Europe/Madrid');
+
 $all_posts = [
     [
         "id" => 1,
@@ -22,43 +25,7 @@ $all_posts = [
     ]
 ];
 
-//loops (while, do, for, foreach)
 
-echo '<hr/>';
-
-$contador = 1;
-while ($contador <= 10) {
-  echo $contador;
-	echo '<br/>';
-	$contador = $contador + 1;
-}
-
-$contador = 10;
-do {
-	echo "Mi contador es $contador";
-	echo '<br/>';
-	$contador = $contador + 1;
-} while ( $contador <= 20 );
-
-$longitud_posts = count( $all_posts );
-for ( $i = 0; $i < $longitud_posts; $i++ ) {
-	echo $all_posts[ $i ]['title'];
-	echo '<br/>';
-}
-
-foreach ( $all_posts as $post ) {
-  echo $post['title'];
-	echo '<br/>';
-}
-
-foreach ( $all_posts as $post ) {
-  echo '<strong>POST:</strong>';
-	echo '<br/>';
-  foreach ( $post as $key => $value ) {
-    echo "- La clave $key tiene valor $value";
-		echo '<br/>';
-  }
-}
 
 ?>
 
@@ -84,24 +51,27 @@ foreach ( $all_posts as $post ) {
 
     <div id="content">
         <div class="posts">
-            <article>
-                <header>
-                    <h2 class="post-title"><?php echo $all_posts[0]["title"]; ?></h2>
-                </header>
-                <div clas="post-content">
-                    <?php echo $all_posts[0]["content"]; ?>
-                </div>
-                <footer></footer>
-            </article>
-            <article>
-                <header>
-                    <h2 class="post-title"><?php echo $all_posts[1]["title"]; ?></h2>
-                </header>
-                <div clas="post-content">
-                    <?php echo $all_posts[1]["content"]; ?>
-                </div>
-                <footer></footer>
-            </article>
+            <?php foreach ($all_posts as $post) {
+                ?>
+                <article>
+                    <header>
+                        <h2 class="post-title"><?php echo $post["title"]; ?></h2>
+                    </header>
+                    <div class="post-content">
+                        <?php echo $post["content"]; ?>
+                    </div>
+                    <footer>
+                        <span class="post-date">
+                            Publicada en:
+                            <?php echo strftime("%d %b %Y"); ?>
+                            <!-- echo strtotime ("2018-01-15" 10:15:00); LO VUELVE PARA QUE PUEDA SER LEIDA LA FECHA -->
+                        </span>
+                    </footer>
+                </article>
+                <?php 
+            } ?>
+            
+           
         </div>
     </div>
 
