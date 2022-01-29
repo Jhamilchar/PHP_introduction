9d6d9d3c2309b7353d752ecab93558f37111a1cc
Jhamilchar <jhamil.arroyo.a@gmail.com>
2022-01-29 16:55:50 -0500

    Estructura de control y operadores(no lo entendi tan bien)

--- tu-proyecto/index.php
+++ tu-proyecto/index.php
@@ -24,7 +24,177 @@ $all_posts = [
         "pusblished_on" => "2018-01-2020",
     ]
 ];
+// OPERADORES
+// Aritméticos
+$x = 10;
+$y = 15;
+var_dump( $x + $y );
+var_dump( $x - $y );
+var_dump( $x * $y );
+var_dump( $x / $y );
 
+// Asignación
+$z = 50;
+$x = $z;
+var_dump( $x );
+
+$x += $y; // Lo mismo que $x = $x + $y
+var_dump( $x );
+
+$x -= $y; // Lo mismo que $x = $x - $y
+var_dump( $x );
+
+$x *= $y; // Lo mismo que $x = $x * $y
+var_dump( $x );
+
+$x /= $y; // Lo mismo que $x = $x / $y
+var_dump( $x );
+
+// De incremento y decremento
+$x++; // Lo mismo que $x = $x + 1
+var_dump( $x );
+
+$x--; // Lo mismo que $x = $x - 1
+var_dump( $x );
+
+// De comparación
+$x = 10;
+$y = '10';
+var_dump( '$x == $y' );
+var_dump( $x == $y );
+var_dump( '$x === $y' );
+var_dump( $x === $y );
+
+$y = 10;
+var_dump( '$x === $y' );
+var_dump( $x === $y );
+
+$x = 10;
+$y = 15;
+var_dump( '$x < $y' );
+var_dump( $x < $y );
+var_dump( '$x > $y' );
+var_dump( $x > $y );
+var_dump( '$x <= $y' );
+var_dump( $x <= $y );
+var_dump( '$x >= $y' );
+var_dump( $x >= $y );
+var_dump( '$x != $y' );
+var_dump( $x != $y );
+var_dump( '$x !== $y' );
+var_dump( $x !== $y );
+
+// De cadenas
+$cadena1 = 'Hola';
+$cadena2 = ' Mundo';
+var_dump( $cadena1 . $cadena2 );
+
+$cadena2 .= $cadena1;
+var_dump( $cadena2 );
+
+// Lógicos
+$x = true;
+$y = false;
+
+// && (AND)
+var_dump( '$x && $y' );
+var_dump( $x && $y );
+
+// || (OR)
+var_dump( '$x || $y' );
+var_dump( $x || $y );
+
+// ! (NOT)
+var_dump( '! $x' );
+var_dump( ! $x );
+var_dump( '! $y' );
+var_dump( ! $y );
+
+// Más ejemplos
+$x = false;
+$y = false;
+var_dump( '$x && $y' );
+var_dump( $x && $y );
+var_dump( '$x || $y' );
+var_dump( $x || $y );
+
+$x = true;
+$y = 1;
+var_dump( '$x && $y' );
+var_dump( $x && $y );
+var_dump( '$x || $y' );
+var_dump( $x || $y );
+
+$x = true;
+$y = false;
+var_dump( '!( $x && $y )' );
+var_dump( ! ( $x && $y ) );
+var_dump( '!( $x || $y )' );
+var_dump( ! ( $x || $y ) );
+
+// If
+$x = 10;
+if ( $x > 5 ) {
+   var_dump( '$x es mayor que 5' );
+}
+
+if ( $x > 15 ) {
+   var_dump( '$x es mayor que 15' );
+}
+
+// If..else
+if ( $x > 15 ) {
+   var_dump( '$x es mayor que 15' );
+}
+else {
+   var_dump( '$x no es mayor que 15' );
+}
+
+$x = true;
+$y = 1;
+if ( $x && $y ) {
+   var_dump( '$x && $y se cumple' );
+}
+
+if ( $x === true && $y == 1 ) {
+   var_dump( '$x === true && $y == 1 se cumple' );
+}
+
+$y =  'Hola';
+if ( $x || $y === 'Hola' ) {
+   var_dump( '$x || $y === \'Hola\' se cumple' );
+}
+
+/**
+* ESTRUCTURAS DE CONTROL
+*/
+// If..elseif..else
+$x = 5;
+if ( $x > 15 ) {
+   var_dump( '$x es mayor que 15' );
+}
+elseif ( $x > 10 ) {
+   var_dump( '$x es mayor que 10' );
+}
+else {
+   var_dump( '$x es menor o igual a 10' );
+}
+
+// Switch..Case
+$x = 'Lucía';
+switch ( $x ) {
+   case 'Ignacio': {
+       var_dump('El nombre es Ignacio' );
+       break;
+   }
+   case 'Lucía': {
+       var_dump('El nombre es Lucía' );
+       break;
+   }
+   default: {
+       var_dump('No se conoce el nombre' );
+   }
+}
 
 
 ?>
